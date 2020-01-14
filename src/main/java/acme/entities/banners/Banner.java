b4cc0b8956149e50b236b2d29b4540942c8783a7
lizseth <lizseth.esquen@gmail.com>
2020-01-14 22:23:30 +0100

    task008 level A DO6 and Rejected auditor

--- src/main/java/acme/entities/banners/Banner.java
+++ src/main/java/acme/entities/banners/Banner.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public abstract class Banner extends DomainEntity {
+public class Banner extends DomainEntity {
 
 	private static final long	serialVersionUID	= 1L;
 
